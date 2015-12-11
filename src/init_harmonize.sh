622403a02bfdc55f4dd4980c9f1da405610aac70
Jaison Brooks <jbrooks@enphaseenergy.com>
2015-12-11 14:38:31 -0700

    removed unneeded line in init

--- src/init_harmonize.sh
+++ src/init_harmonize.sh
@@ -11,13 +11,11 @@ sleep 0.3
 echo "$L Moving [ harmonize.rb ] to /usr/local/bin" | sed $'s/harmonize.rb/\e[36m&\e[0m/' 
 sleep 0.3
 cp -r harmonize.rb /usr/local/bin/harmonize
-echo "$L Now you can now exec the [ harmonize ] command from within your path :)" | sed $'s/harmonize/\e[36m&\e[0m/' 
-sleep 0.3
 echo "$L Execute \`harmonize -h\` to get started" | sed $'s/harmonize -h/\e[34m&\e[0m/' 
 sleep 0.3
-echo "$L Have Fun Harmonizing your digital life!" | sed $'s/Harmonizing/\e[31m&\e[0m/'
+echo "$L Have Fun Harmonizing your digital life!" | sed $'s/Harmonizing/\e[35m&\e[0m/'
 sleep 0.3
 # echo "$L"
-echo "$L Created By: Jaison Brooks" | sed $'s/Jaison Brooks/\e[35m&\e[0m/'
+echo "$L Created By: Jaison Brooks" | sed $'s/Jaison Brooks/\e[31m&\e[0m/'
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 # Might switch to sym link ln -s $PWD/harmonize /usr/local/bin
